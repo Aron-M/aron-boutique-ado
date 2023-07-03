d43076c2b325d6a966ec1e7f0bf7f76a1e8bc51b
Aron-M <aron.moelich88@gmail.com>
2023-07-03 21:27:02 +0000

    WIP not yet rendering 'products' dynamically from all_products view

--- products/models.py
+++ products/models.py
@@ -17,8 +17,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
