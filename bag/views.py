e389964911c2f5d9f058072f3cbd7f757fba3d78
Aron-M <aron.moelich88@gmail.com>
2023-07-11 20:29:12 +0000

    WIP Adding Products 1

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,10 @@ def view_bag(request):
     """ A view to return bag contents page """
     
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add quantity of specified product to shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
