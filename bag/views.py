09bb3c90baed109f5bbb731862434d6d4a6b6a88
Aron-M <aron.moelich88@gmail.com>
2023-07-16 08:27:05 +0000

    finnished product size logic

--- bag/views.py
+++ bag/views.py
@@ -13,8 +13,8 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
-    if 'product.size' in request.POST:
-        size = request.POST['product_size']  
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if size:
@@ -24,8 +24,8 @@ def add_to_bag(request, item_id):
             else:
                 bag[item_id]['items_by_size'][size] = quantity
         else:
-            bag[item_id] = {'items_by_size': {size.quantity}}
-    else:    
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
