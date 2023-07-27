f4d71526b2b54e3a2459dca03221b7572a73f1c4
Aron-M <aron.moelich88@gmail.com>
2023-07-27 09:13:36 +0000

    Stripe video 3

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,40 @@
+let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+let client_secret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripe_public_key);
+let elements = stripe.elements();
+
+let style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+let card = elements.create('card', {'style': style});
+card.mount('#card-element');
+
+
+//Handle realtime validation errors on card element
+card.addEventListener('change',function (event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContext = '';
+    } 
+});
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NYBbGBUqkswIymkiNneN1yZ4ohRjxnMWsJCQVC3JymwTxXyEkxcNZ4gWQXcFNXOcKNF0Su4TKXft13HI97SSxBF008fmQ2ySj',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
