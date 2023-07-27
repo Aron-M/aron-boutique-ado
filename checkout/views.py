7aba4c3c89918fb67281099f772bc74fa59e635e
Aron-M <aron.moelich88@gmail.com>
2023-07-27 16:55:43 +0000

    added basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.conf import settings
+
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
 import stripe
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -22,14 +27,14 @@ def checkout(request):
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
+        currency=settings.STRIPE_CURRENCY,
     )
 
     order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(request,'Stripe Public Key is missing. \
-             Please set it in your evironment')
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
